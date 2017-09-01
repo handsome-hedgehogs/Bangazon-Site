6e5531fcaec353ba47ab74eabd516d1d085f2f40
Tamela Lerma <Tlerma83@gmail.com>
2017-09-01 15:36:19 -0700

    added class for MyProductsViewModel

---
+++ BangazonAuth/Classes/SoldProductsGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAuth.Models
+{
+    public class SoldProductsGroup
+    {
+        public int ProdQuantity { get; set; }
+        public int ProdId { get; set; }
+        public string ProdTitle { get; set; }
+        public int ProdCount { get; set; }
+        public int ProdUnSold { get; set; }
+        public IEnumerable<Product> SoldProductsList { get; set; }
+    }
+}
--- BangazonAuth/Models/ProductViewModels/MyProductsViewModel.cs
+++ BangazonAuth/Models/ProductViewModels/MyProductsViewModel.cs
@@ -11,9 +11,10 @@ namespace BangazonAuth.Models.ProductViewModels
     {
         public IEnumerable<Product> Products { get; set; }
 
-        public List<IGrouping<int, Product>> SoldProd { get; set; }
+        public List<SoldProductsGroup> SoldProd { get; set; }
 
         public virtual ApplicationUser User { get; set; }
+        public int NotSold = 0;
 
         public MyProductsViewModel(ApplicationDbContext context, ApplicationUser user)
         {
@@ -24,15 +25,27 @@ namespace BangazonAuth.Models.ProductViewModels
                         join o in context.Order
                         on ot.OrderId equals o.OrderId
                         where o.PaymentType != null
-                        group new { p, ot } by new { p.Title, p.Quantity } into grouped
-                        select new
+                        group new { p, p.ProductId, ot } by new { p.Title, p.ProductId, p.Quantity } into grouped
+                        select new SoldProductsGroup
                         {
-                            Title = grouped.Key.Title,
-                            Quantity = grouped.Key.Quantity,
-                            TimesOrdered = grouped.Select(g => g.ot.ProductId).Count()
+                            ProdTitle = grouped.Key.Title,
+                            ProdId = grouped.Key.ProductId,
+                            ProdQuantity = grouped.Key.Quantity,
+                            ProdCount = grouped.Select(g => g.ot.ProductId).Count()
                         }
                         ).ToList();
 
+            //Products = from p in context.Product
+            //           where p.User == user
+            //           join s in SoldProd
+            //           on p.ProductId equals s.ProdId
+            //           group new { p.Title, p.Quantity, s.}
+                       
+            //           select p;
+
+            //NotSold = from p in Products
+            //          join s in SoldProd
+            //          on 
         }
     }
 }
